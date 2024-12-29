04e1015ff67c24565ae991982c84a7963f7c30ee
jefferson-gbarbosa <jeffersonqx@gmail.com>
2024-12-29 13:40:33 -0300

    :zap: perf: Adição do campo confirmação de senhas

--- src/pages/reset-password.tsx
+++ src/pages/reset-password.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { AxiosError } from 'axios';
 import { Button } from '../components/button';
 import { PasswordCheckList } from '../components/password-check-list';
-import { Eye, EyeOff, LockKeyhole } from 'lucide-react';
+import { LockKeyhole } from 'lucide-react';
 import { toast } from 'react-toastify';
 
 const ResetPasswordSchema = z.object({
@@ -18,7 +18,7 @@ const ResetPasswordSchema = z.object({
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/[0-9]/, "Password must contain at least one number")
-    .regex(/[\W_]/, "Password must contain at least one special character"),
+    .regex(/[\W_]/, "Password must contain at least one special character"), 
 });
 
 type ResetPasswordSchema = z.infer<typeof ResetPasswordSchema>;
@@ -28,19 +28,21 @@ export function ResetPassword() {
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
     const [password, setPassword] = useState<string>('');
-    const [showPassword, setShowPassword] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState("");
+  
     const { register, handleSubmit, formState: { errors } } = useForm<ResetPasswordSchema>({
         resolver: zodResolver(ResetPasswordSchema)
     });
     const { token } = useParams<{ token: string }>();
-
-    const togglePasswordVisibility = () => {
-        setShowPassword(prev => !prev);
-    };
-
     const onSubmit = async (data: ResetPasswordSchema) => {
         setLoading(true);
         setError(null);
+        
+        if(data.password !== confirmPassword){
+           setError('Passwords do not match')
+           setLoading(false);
+           return
+        }
 
         try {
             const res = await api.post(`/reset-password/${token}`, data);
@@ -77,35 +79,45 @@ export function ResetPassword() {
                 className="p-8 fixed max-w-md w-full top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 bg-white rounded-xl border border-solid border-[#11181C]"
                 onSubmit={handleSubmit(onSubmit)}
             >
-                <h2 className='text-3xl mb-0.5 border-[#11181C]'>Reset account password</h2>
+                <h2 className='text-3xl mb-0.5 border-[#11181C]'>Reset Password</h2>
                 <p className='inline-block text-base mb-4 text-[#687076]'>Enter a new password for noreply@shopify.com</p>
-                {error && <div className="text-red-600 mb-4">{error}</div>}
-                
-                <Form.Field name='password' className='mb-6'>
+                <Form.Field name='password' className='mb-4'>
                     <Form.Label className='text-[#11181C]'>Password</Form.Label>
                     <div className='relative'>
                         <LockKeyhole size={20} className={`absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500 ${errors.password ? 'text-red-600' : ' text-gray-500'}`}/>
-                        <div className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={togglePasswordVisibility}>
-                            {showPassword ? <Eye size={20} className="text-gray-500" /> : <EyeOff size={20} className="text-gray-500" />}
-                        </div>
                         <input
                             {...register("password", { required: "Password is required" })}
                             className={`box-border text-[#687076] inline-flex h-[44px] w-full appearance-none items-center justify-center rounded px-10 outline-none border border-solid border-[#687076] caret-zinc-500 ${errors.password ? 'border-red-600' : ''}`}
-                            type={showPassword ? "text" : "password"}
+                            type={"password"}
                             placeholder="Enter your new password"
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    
                     {errors.password && <span className="text-red-600">{errors.password.message}</span>}
+                </Form.Field>
+                <Form.Field name='password' className='mb-6'>
+                    <Form.Label className='text-[#11181C]'>Confirm Password</Form.Label>
+                    <div className='relative'>
+                        <LockKeyhole size={20} className={`absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500 ${errors.password ? 'text-red-600' : ' text-gray-500'}`}/>
+                        <input
+                            className={`box-border text-[#687076] inline-flex h-[44px] w-full appearance-none items-center justify-center rounded px-10 outline-none border border-solid border-[#687076] caret-zinc-500 ${errors.password ? 'border-red-600' : ''}`}
+                            type={"password"}
+                            placeholder="Confirm your new password"
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </div>
                 </Form.Field>
                 <div className='mb-6 px-2.5'>
                   <PasswordCheckList password={password}/>
                 </div>
+                
                 <Form.Submit asChild className='mb-6'>
-                    <Button  disabled={loading} text = {loading ? 'Updating...' : 'Update'} />
+                    <Button  disabled={loading} text = {loading ? "Resetting..." : "Set New Password"} />
                 </Form.Submit>
+                {error && <div className="text-red-600 mt-4 text-center">{error}</div>} 
             </Form.Root>
         </>
     );
 }
+
+
