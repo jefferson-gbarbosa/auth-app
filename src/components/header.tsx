29230fcf349c7c83036477ac040daf5396236129
jefferson-gbarbosa <jeffersonqx@gmail.com>
2025-01-22 14:57:47 -0300

    :recycle: refactor: Organização dos links das mídias sociais

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 import { Link, useLocation } from 'react-router-dom';
-import { List } from './list';
+// import { List } from './list';
 import { Logo } from './logo';
 import { ButtonLogout } from './button-logout';
 import { User } from './user';
 import { useEffect, useState } from 'react';
+import { RiGithubFill, RiLinkedinFill } from 'react-icons/ri';
 
 export function Header(){
     const location = useLocation();  
@@ -27,7 +28,20 @@ export function Header(){
                 </Link>
                 {!isLoginPage && (
                     <ul className="flex justify-between items-center flex-col md:flex-row w-full md:w-auto mt-4 md:mt-0">
-                     {isLoggedIn?  <User /> :  <List/>}
+                     {isLoggedIn?  <User /> :  
+                        <>
+                            <li className="ml-0 mb-3 md:mb-0 md:ml-3">
+                                <Link className="text-white text-2xl md:text-3xl" to="https://www.linkedin.com/in/jefferson-gbarbosa/" target="_blank" rel="noopener noreferrer">
+                                    <RiLinkedinFill />
+                                </Link>
+                            </li>
+                            <li className="ml-0 mb-3 md:mb-0 md:ml-3">
+                                    <Link className="text-white text-2xl md:text-3xl" to="https://github.com/jefferson-gbarbosa" target="_blank" rel="noopener noreferrer">
+                                        <RiGithubFill />
+                                    </Link>
+                            </li>
+                        </>
+                     }
                      {isLoggedIn ? <ButtonLogout setIsAuthenticated={setIsLoggedIn}/> : 
                         <Link to='/login'
                            className="ml-0 md:ml-3 mt-2 md:mt-0 p-2.5 text-[#63b68c] text-lg font-bold md:text-xl outline-none rounded-lg border border-[#63b68c] border-solid cursor-pointer w-9/12 md:w-auto text-center md:text-left"
